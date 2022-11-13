2a0d316940e019ff25c43c787872282f08f9760b
BuildTools <unconfigured@null.spigotmc.org>
2022-11-13 03:36:06 -0600

    Database Addition and Render Adjustments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import React from 'react'
 import GoogleMapReact from 'google-map-react';
+import {useState} from 'react';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBgxs2tW6Lb0GaJtfSCdeQyVd--WrzSBRs",
+  authDomain: "hacka-e917c.firebaseapp.com",
+  projectId: "hacka-e917c",
+  storageBucket: "hacka-e917c.appspot.com",
+  messagingSenderId: "327466742979",
+  appId: "1:327466742979:web:86fdde1df3fbae676f5e76"
+};
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
 
 const AnyReactComponent = ({ text, color }) => (
   <div style={{
@@ -43,24 +58,28 @@ class SimpleMap extends React.Component {
   }
 }
 
-
-ReactDOM.render(
-  <div style={{width: '600px', height: '600px'}}>
-    <SimpleMap/>
-  </div>,
-  document.getElementById('home')
-);
+createRoot(document.getElementById('home')).render(<div style={{width: '600px', height: '600px'}}>
+<SimpleMap/>
+</div>);
 
 
-
-
-
-
-const App = () => 
-{
-    return <h1>This is my app!</h1>;
+const read = async() => {
+  const docRef = db.collection("Data2022").doc("Arrays");
+  const docSnap = await docRef.get();
+  if (docSnap) {
+    console.log("Document data:", docSnap.data());
+  } else {
+    console.log("No such document!");
+  }
 }
+read();
 
-ReactDOM.render(<App />, document.getElementById('app'));
+
+
+const Text = () => 
+{
+    return <h1>Affordable Housing Forecaster</h1>;
+}
+createRoot(document.getElementById('text')).render(<Text />);
 
 
